616c48e1ce0be0429145e1fa1724dc4b9b393f43
Drako <dragon12xw@gmail.com>
2021-04-06 20:15:48 -0500

    mas opciones add

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -16,15 +16,34 @@ export class AdminComponent implements OnInit {
   inputimg!: ElementRef;
 
   public modalClass:string = "bg"
-  passp= 'admin';
-  mod='';
+  passp = 'admin';
+  mod = '';
+  opciones = "";
 
   public uploadPercent!:any //Observable<any>;
+  public uploadPercent1!:any
+  public uploadPercent2!:any
+  public uploadPercent3!:any
+  public uploadPercent4!:any
+  public uploadPercent5!:any
   public downloadURL!: Observable<string>;
   linnkImg!:string;
   articulos!:any;
   articulo = {
-    subtitulo : '', pagina: '', indice: '', titulo: '', descripcion:'', precio: '', tipo: ''
+    subtitulo : '', pagina: '', indice: '', titulo: '', descripcion:'', precio: '', tipo: '',color:'',
+    mas:{
+      masOpc: '',
+      linkImg1: '',
+      desc1: '',
+      linkImg2: '',
+      desc2: '',
+      linkImg3: '',
+      desc3: '',
+      linkImg4: '',
+      desc4: '',
+      linkImg5: '',
+      desc5: '',
+    }
   }
 
 
@@ -52,8 +71,69 @@ export class AdminComponent implements OnInit {
     ).subscribe()
   }
 
+  handleImg1(ev:any): void {
+    this.picthSer.uploadImage(ev)
+    this.uploadPercent = this.picthSer.task.percentageChanges();
+    this.picthSer.task.snapshotChanges().pipe(
+      finalize(() => {
+        this.picthSer.fileRef.getDownloadURL().subscribe((urlImage: string) => {
+          this.articulo.mas.linkImg1 = urlImage
+        })
+      })
+    ).subscribe()
+  }
+
+  handleImg2(ev:any): void {
+    this.picthSer.uploadImage(ev)
+    this.uploadPercent = this.picthSer.task.percentageChanges();
+    this.picthSer.task.snapshotChanges().pipe(
+      finalize(() => {
+        this.picthSer.fileRef.getDownloadURL().subscribe((urlImage: string) => {
+          this.articulo.mas.linkImg2 = urlImage
+        })
+      })
+    ).subscribe()
+  }
+
+  handleImg3(ev:any): void {
+    this.picthSer.uploadImage(ev)
+    this.uploadPercent = this.picthSer.task.percentageChanges();
+    this.picthSer.task.snapshotChanges().pipe(
+      finalize(() => {
+        this.picthSer.fileRef.getDownloadURL().subscribe((urlImage: string) => {
+          this.articulo.mas.linkImg3 = urlImage
+        })
+      })
+    ).subscribe()
+  }
+
+  handleImg4(ev:any): void {
+    this.picthSer.uploadImage(ev)
+    this.uploadPercent = this.picthSer.task.percentageChanges();
+    this.picthSer.task.snapshotChanges().pipe(
+      finalize(() => {
+        this.picthSer.fileRef.getDownloadURL().subscribe((urlImage: string) => {
+          this.articulo.mas.linkImg4 = urlImage
+        })
+      })
+    ).subscribe()
+  }
+
+  handleImg5(ev:any): void {
+    this.picthSer.uploadImage(ev)
+    this.uploadPercent = this.picthSer.task.percentageChanges();
+    this.picthSer.task.snapshotChanges().pipe(
+      finalize(() => {
+        this.picthSer.fileRef.getDownloadURL().subscribe((urlImage: string) => {
+          this.articulo.mas.linkImg5 = urlImage
+        })
+      })
+    ).subscribe()
+  }
+
+
   addArticulos(){
-    const { pagina, indice, titulo, subtitulo, descripcion, precio, tipo } = this.articulo
+    const { pagina, indice, titulo, subtitulo, descripcion, precio, tipo,color, mas } = this.articulo
     const newData = {
       indice,
       pagina,
@@ -62,7 +142,9 @@ export class AdminComponent implements OnInit {
       precio,
       subtitulo,
       link: this.linnkImg,
-      descripcion
+      descripcion,
+      color,
+      mas
     }
     this.picthSer.postArticulo(newData).then(() => {
       this.clear()
@@ -70,7 +152,20 @@ export class AdminComponent implements OnInit {
   }
 
   clear(){
-    this.articulo = {pagina: '', indice: '', titulo: '', subtitulo : '', descripcion:'', precio:'', tipo:''}
+    this.articulo = {pagina: '', indice: '', titulo: '', subtitulo : '', descripcion:'', precio:'', tipo:'',color:'',
+    mas:{
+      masOpc: '',
+      linkImg1:'',
+      desc1:'',
+      linkImg2:'',
+      desc2:'',
+      linkImg3:'',
+      desc3:'',
+      linkImg4:'',
+      desc4:'',
+      linkImg5:'',
+      desc5:'',
+    }}
     this.inputimg.nativeElement.value = ''
     this.linnkImg = '';
     this.uploadPercent = null
